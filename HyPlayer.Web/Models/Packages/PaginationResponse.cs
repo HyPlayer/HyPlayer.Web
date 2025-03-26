c3ad26a995ca3e646dcf80c9f0bbd422845903cd
Kengwang <github@kengwang.com.cn>
2025-03-26 22:20:36 +0800

    chore: add missing model

---
+++ HyPlayer.Web/Models/Packages/PaginationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace HyPlayer.Web.Models.Packages;
+
+public class PaginationResponse<T>
+{
+    public T[] Data { get; set; } = [];
+    public int Total { get; set; }
+}
---
+++ HyPlayer.Web/Models/Packages/ReleaseCreateRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using HyPlayer.Web.Models.DbModels;
+
+namespace HyPlayer.Web.Models.Packages;
+
+public class ReleaseCreateRequest
+{
+    [JsonPropertyName("channels")]
+    public ChannelType[] Channels { get; set; } = [];
+    
+    [JsonPropertyName("version")]
+    [MaxLength(30)] public required string Version { get; set; }
+    
+    [JsonPropertyName("date")]
+    public DateTime ReleaseDate { get; set; } = DateTime.Now;
+    
+    [JsonPropertyName("note")]
+    public required string ReleaseNotes { get; set; }
+    
+    [JsonPropertyName("url")]
+    public required string DownloadUrl { get; set; }
+    
+    [JsonPropertyName("authKey")]
+    public required string AuthKey { get; set; }
+}
